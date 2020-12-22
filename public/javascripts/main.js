b38cc17a89015c061c11ecefbe71414995328c9f
Thaqib <thaqibdamani0@gmail.com>
2020-12-23 01:31:09 +0530

    First

---
+++ public/javascripts/main.js
@@ -0,0 +1,193 @@
+
+var pointLst = []; 
+/*
+Some variables, including a canvas for drawing, a drawing context for
+the canvas, a flag (boolean) to indicate drawing, x and y points for drawing, and a
+final flag (dot_flag, boolean) to support starting a new stroke by clearing contents.
+*/
+var canvas, drawdiv, ctx, flag = false,
+    prevX = 0,
+    currX = 0,
+    prevY = 0,
+    currY = 0,
+    dot_flag = false;
+
+/*
+Horrible variable names: Do as I do, not as I say. x is your drawing color and y is the 
+stroke width when drawing. So a black stroke that is 2 pixels wide.
+*/
+var x = "black",
+    y = 2;
+
+/*
+function init gets called on launch. It sets up the drawing context for the canvas, ctx and
+it adds "listeners" to the canvas to allow the canvast to receive mouse events. Mouse events 
+will be handled by a function findxy. Note that findxy gets two aprameters, the "event", i.e. 
+was it a move, a mousedown, a mouseup, or a mouseout event, and the event parameters, "e" which
+has useful things like x,y components of the mouse.
+*/
+function init() {
+    canvas = document.getElementById('can');
+    drawdiv = document.getElementById('sketch');
+    ctx = canvas.getContext("2d");
+    w = canvas.width;
+    h = canvas.height;
+
+    canvas.addEventListener("mousemove", function (e) {
+        findxy('move', e)
+    }, false);
+    canvas.addEventListener("mousedown", function (e) {
+        findxy('down', e)
+    }, false);
+    canvas.addEventListener("mouseup", function (e) {
+        findxy('up', e)
+    }, false);
+    canvas.addEventListener("mouseout", function (e) {
+        findxy('out', e)
+    }, false);
+}
+
+/*
+This helper function gets contents from the Clipboard. It will typically prompt you for permissionto read from the clipboard. When it gets content from the clipboard, it will 
+paste it into the points list and it will draw the points on the screen.
+*/
+async function getClipboardContents(){
+    try{
+        const text = await navigator.clipboard.readText();
+        document.getElementById("points").textContent = text;
+        draw_all();
+    } catch (err){
+        console.error('Failed');
+    }
+}
+
+/*
+    Record points *** MAIN *******
+*/
+function draw() {
+    ctx.beginPath();
+    ctx.moveTo(prevX, prevY);
+    ctx.lineTo(currX, currY);
+    console.log(pointLst);
+    ctx.strokeStyle = x;
+    ctx.lineWidth = y;
+    ctx.stroke();
+    ctx.closePath();
+    document.getElementById("points").textContent += '['+currX+','+currY+'],';
+}
+
+/*
+This function is a bit complex. It basically parses a list of points and draws the 
+entire list of points. It does this by filling an "array", a "list" in racket using
+a strong parsing function that strips everything out of the string except numbers
+(that's the allpoints = mystring ... line) and then goes through and draws all of 
+those points (that's the for loop).
+*/
+function draw_all() {
+    var mystring = document.getElementById("points").textContent;
+    var allpoints = mystring.match(/(\+|-)?((\d+(\.\d+)?)|(\.\d+))/g).map(Number);
+    if ((allpoints.length > 1) && ((allpoints.length % 2) == 0)){
+        var i;
+        ctx.beginPath();
+        ctx.moveTo(allpoints[0], allpoints[1]);
+        for (i=1; i<(allpoints.length/2); i++) {
+            ctx.lineTo(allpoints[2*i], allpoints[2*i+1]);
+            ctx.strokeStyle = x;
+            ctx.lineWidth = y;
+            ctx.stroke();
+        }
+        ctx.closePath();
+        create_image()
+    }
+}
+
+/*
+Clear everything.
+*/
+function erase() {
+    ctx.clearRect(0, 0, w, h);
+    document.getElementById("canvasimg").style.display = "none";
+    document.getElementById("points").textContent = '';
+}
+
+/*
+Copies the points drawn on the display to the clipboard. You can use this to then paste your list of points into Dr Racket.
+*/
+function copy() {
+    document.getElementById("points").select();
+    document.execCommand('copy');
+    // Use RE to convert Txt -> Array 
+}
+
+/*
+A past wrapper function. Calls the getClipboardContents function to do the work.
+*/
+function paste() {	
+    erase();
+    getClipboardContents();
+}
+
+/*
+Creates a small image of the stroke you have drawn. You can use your Racket code to create
+a recognizer for any set of gestures you want (e.g. the numbers 1 to 10, etc.). If you do this
+you might want little "images" that you can paste somewhere to show people what the gestures
+your recognizer accepts look like.
+*/
+function create_image() {
+    document.getElementById("canvasimg").style.border = "2px solid";
+    var dataURL = canvas.toDataURL();
+    document.getElementById("canvasimg").src = dataURL;
+    document.getElementById("canvasimg").style.display = "inline";
+}
+
+/*
+this is the function that handles mouse events. Basically it reads as follows:
+1. If the event is a mousedown event (the user presses down on the mouse button),
+then we clear the rectangle and start a new points list. We also set previous x and y
+to the current position, and we get ready to draw a line by setting our flags to true.
+We also create a small dot, a rectangle, of size 2, 2 at the current point on the 
+beginning of the line. 
+2. If the even is up or out (i.e. we released the mouse button or moved out of the
+region), then we want to stop drawing. We also show the image of what was drawn.
+3. Otherwise, it is a mousemove function. If we are drawing (i.e. the flag is true, set 
+on mouse down, the first if statement above), then we set our points and call the draw 
+function.
+*/
+function findxy(res, e) {
+    if (res == 'down') {
+        ctx.clearRect(0, 0, w, h);
+        document.getElementById("points").textContent = '[';
+        prevX = currX;
+        prevY = currY;
+        currX = e.clientX - canvas.offsetLeft;
+        currY = e.clientY - (canvas.offsetTop + drawdiv.offsetTop - window.pageYOffset);
+
+        flag = true;
+        dot_flag = true;
+        if (dot_flag) {
+            ctx.beginPath();
+            ctx.fillStyle = x;
+            ctx.fillRect(currX, currY, 2, 2);
+            ctx.closePath();
+            dot_flag = false;
+        }
+    }
+    if (res == 'up' || res == "out") {
+        if (flag)
+        {
+            document.getElementById("points").textContent += ']\n';
+            create_image();
+        }
+        flag = false;
+    }
+    if (res == 'move') {
+        if (flag) {
+            prevX = currX;
+            prevY = currY;
+            currX = e.clientX - canvas.offsetLeft;
+            currY = e.clientY - (canvas.offsetTop + drawdiv.offsetTop - window.pageYOffset);
+            draw();
+        }
+    }
+}
+
---
+++ utils/gesture_rec.js
@@ -0,0 +1,134 @@
+var data = require('../data/gesture_templates.json'); 
+
+// Constants for max/min coordinates. 
+let MAX_X = 500;
+let MAX_Y = 500;
+let MIN_X = 0; 
+let MIN_Y = 0; 
+
+console.log(data);
+
+var testArr = [[3,3], [1,1],[1,2]];
+var diamond = [[100,0], [200, 100], [100, 200], [0,100],[100,0]]; 
+// REC Functions. 
+var getX = (pt) => pt[0];
+var getY = (pt) => pt[1];
+
+
+// Translate Gesture, translates a gesture G by x-offset and y-offset
+var translate = (G,x,y) => G.map((pt)=>[getX(pt)+x, getY(pt)+y]);
+
+
+// Scale Gesture, scales a gesture by x in x direction and y in y direction
+var scale = (G,x,y) => G.map((pt)=>[getX(pt)*x, getY(pt)*y]);
+
+
+// Bounding box, get the bounding box for gesture G. (Difference of max and min coordinates). 
+var Bbox = (G) =>{
+    var max_x = G.reduce((acc,v) =>  Math.max(acc, getX(v)), MIN_X);
+    var min_x = G.reduce((acc,v) =>  Math.min(acc, getX(v)), MAX_X);
+
+    var max_y = G.reduce((acc,v) =>  Math.max(acc, getY(v)), MIN_Y);
+    var min_y = G.reduce((acc,v) =>  Math.min(acc, getY(v)), MAX_Y);
+
+    
+    return [[min_x,min_y], [max_x, max_y]]; 
+}
+
+// Point Dist: 
+var pointDist = (a,b) => Math.sqrt((getX(b)-getX(a))**2 + (getY(b)-getY(a))**2); 
+    
+
+// Gesture Len, get the length of the gesture.  
+function GestureLength(G){
+
+    if(G.length == 0 || G.length == 1){
+        return 0; 
+    }
+    else {
+        return (pointDist(G[0], G[1]) + GestureLength(G.slice(1))); 
+    }
+}
+
+
+var getPts = (G, Lst) => Lst.map((i)=> G[i]);   
+
+// Five Sample, get the the first, n/4th, n/2th, 3n/4th, and last point.
+var FiveSample = (G) => {
+    let n = G.length; 
+    return getPts(G, [0, Math.floor(0.25*n), Math.floor(0.5*n), Math.floor(0.75*n), n-1]); 
+}
+
+
+// Move gesture to Origin and scale by sx,sy
+var MoveAndScale = (G,sx,sy) => scale(translate(G, -1*getX(Bbox(G)[0]), -1*getY(Bbox(G)[0])), sx, sy); 
+
+
+// Standard Size of Samples: 
+let minW = 30; 
+let minH = 30; 
+let normSize = 200; 
+
+var vertical_test = (G)=> {
+    return	getX(Bbox(MoveAndScale(G,1,1))[1]) < minW;
+}
+
+var horizontal_test = (G)=> {
+    return	getY(Bbox(MoveAndScale(G,1,1))[1]) < minH;
+}
+
+var get_scale = (G,n) => [n/(getX(Bbox(G)[1])), n/(getY(Bbox(G)[1]))];
+
+
+/** Normalizing gesture */
+function normalize(G){
+    if(vertical_test(G)){
+        return MoveAndScale(MoveAndScale(G,1,1),1,get_scale(G,normSize)[1]); 
+    }
+    else if(horizontal_test(G)){
+        return MoveAndScale(MoveAndScale(G,1,1),get_scale(G,normSize)[0], 1); 
+    }
+    else {
+        // Scale both ways
+        return MoveAndScale(MoveAndScale(G,1,1), get_scale(G,normSize)[0], get_scale(G,normSize)[1]); 
+    }
+}
+
+/**Finds the distance bewteen 2 gestures G1,G2*/
+// requires: |G1| = |G2|
+function Gesture_distance(G1,G2){
+    if(G1.length==0){
+        return 0; 
+    }
+    else{
+        
+        return pointDist(G1[0], G2[0]) + Gesture_distance(G1.slice(1),G2.slice(1)); 
+    }
+}
+
+
+// Basic 5 sample matching function 
+var geometric_5match = (G1, G2) =>  Gesture_distance(normalize(FiveSample(G1)), normalize(FiveSample(G2)))/5; 
+
+const testGesture = [[237,62],[234,62],[229,60],[225,60],[217,58],[208,57],[198,56],[188,56],[180,56],[172,57],[164,60],[158,62],[150,64],[140,69],[136,72],[133,75],[127,82],[122,89],[119,95],[116,108],[116,117],[116,127],[117,141],[120,150],[123,160],[127,168],[135,179],[142,186],[149,194],[162,202],[170,206],[179,208],[196,210],[206,210],[215,208],[237,205],[247,201],[258,197],[265,193],[267,191],[268,191],[269,190],[269,190],[269,189],[269,187],[269,186],[269,184],[268,180],[267,179],[267,177],[266,177],[265,176],[265,176]]; 
+
+console.log(geometric_5match(testGesture,data.Datbase[0].template)); 
+console.log(geometric_5match(testGesture,data.Datbase[1].template)); 
+console.log(geometric_5match(testGesture,data.Datbase[2].template)); 
+console.log(geometric_5match(testGesture,data.Datbase[3].template)); 
+
+function five_point_rec(G){
+    var dist_list = data.Datbase.map((x) => [x.letter, geometric_5match(G, x.template)]); 
+    
+    var curr_min = dist_list[0]; 
+    for (let index = 0; index < dist_list.length; index++) {
+        if (dist_list[index][1] < curr_min[1]) {
+            curr_min = dist_list[index]; 
+        }
+    }
+    return curr_min[0]; 
+}
+console.log(five_point_rec(testGesture)); 
+
+
+    
